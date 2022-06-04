771a69fc000ede7a74dc6d13ad03c58fc03de048
NgTuanLoc <18521011@gm.uit.edu.vn>
2022-06-04 10:49:30 +0700

    remove useless import

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,8 +1,7 @@
-import React from 'react'
-import styled from 'styled-components'
+import styled from "styled-components";
 const Footer = () => {
-  return <h4>footer</h4>
-}
+  return <h4>footer</h4>;
+};
 
 const Wrapper = styled.footer`
   height: 5rem;
@@ -26,6 +25,6 @@ const Wrapper = styled.footer`
   @media (min-width: 776px) {
     flex-direction: row;
   }
-`
+`;
 
-export default Footer
+export default Footer;

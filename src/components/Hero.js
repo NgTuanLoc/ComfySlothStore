c569625f7b7b8704d5923fcdd957903d6a1fd12d
NgTuanLoc <18521011@gm.uit.edu.vn>
2022-06-06 20:29:55 +0700

    Homepage Hero cmp finished

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -4,7 +4,29 @@ import heroBcg from "../assets/hero-bcg.jpeg";
 import heroBcg2 from "../assets/hero-bcg-2.jpeg";
 
 const Hero = () => {
-  return <h4>hero</h4>;
+  return (
+    <Wrapper className="section-center">
+      <article className="content">
+        <h1>
+          design your <br />
+          comfort zone
+        </h1>
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tenetur
+          neque necessitatibus corrupti nam et dolorum, assumenda sunt ipsum
+          sint fugit sed voluptatum facere, cupiditate perspiciatis beatae
+          dolore. Dolor, asperiores soluta! Debitis, animi!
+        </p>
+        <Link to="/products" className="btn hero-btn">
+          Shop now
+        </Link>
+      </article>
+      <article className="img-container">
+        <img src={heroBcg} alt="table" className="main-img" />
+        <img src={heroBcg2} alt="worker" className="accent-img" />
+      </article>
+    </Wrapper>
+  );
 };
 
 const Wrapper = styled.section`
